580c5c70ab6788b3829cd007b187af6a906fa781
Mate Tukacs <matt.tukacs@gmail.com>
2017-02-24 15:24:51 +0100

    test

---
+++ config.js
@@ -0,0 +1,3 @@
+exports.creds = {
+  mongoose_auth: 'mongodb://heroku_xjcf432g:5jrl1mbdgpjbsb2cjqnolpn6tf@ds157799.mlab.com:57799/heroku_xjcf432g'
+}
---
+++ index.js
@@ -0,0 +1,23 @@
+const restify = require('restify');  
+const server = restify.createServer();
+const mongoose = require('mongoose/');
+const config = require('./config');
+const db = mongoose.connect(config.creds.mongoose_auth),
+const Schema = mongoose.Schema;
+
+server.use(restify.bodyParser());
+
+var ArtistSchema = new Schema({
+  id: ObjectId,
+  name: String,
+  genres: Array,
+  albums: Array
+});
+
+const Artist = mongoose.model('Artist', ArtistSchema); 
+
+var newArtist = new Artist({ 
+	id: "adadas",
+  	name: "John Lenon",
+  	genres: [g1, g2, g3],
+  	albums: [a1, a2, a3] });
