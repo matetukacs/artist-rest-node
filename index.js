8568acd2bcf1ee72b2036bbc7b50d71b0f0cc124
Mate Tukacs <matt.tukacs@gmail.com>
2017-02-26 22:29:21 +0100

    12

--- index.js
+++ index.js
@@ -36,6 +36,22 @@ newArtist.save();
 //     , url: '/artists'
 // });
 
-server.get('/a', (req, res, next) => {
+server.get('/artists', (req, res, next) => {
+	Artist.find().exec().then(res.send);
+});
+
+server.get('/artist', (req, res, next) => {
+	Artist.find().exec().then(console.log);
+});
+
+server.put('/artist', (req, res, next) => {
+	Artist.find().exec().then(console.log);
+});
+
+server.post('/artist', (req, res, next) => {
+	Artist.find().exec().then(console.log);
+});
+
+server.delete('/artist', (req, res, next) => {
 	Artist.find().exec().then(console.log);
 });
