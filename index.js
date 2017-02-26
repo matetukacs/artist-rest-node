cd338c2fbe7be3c32013051ea5970b8228a2f80d
Mate Tukacs <matt.tukacs@gmail.com>
2017-02-26 23:44:00 +0100

    12

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ var newArtist = new Artist({
 newArtist.save();
 
 
-server.get('/artists', (a, res, next) => {
+server.get('/artist', (a, res, next) => {
 	console.log("")
 	Artist.find().exec().then(res.send);
 });
