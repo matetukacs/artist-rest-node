3b4a962f309f71ce7b0770a3760481bb1338056b
Mate Tukacs <matt.tukacs@gmail.com>
2017-02-27 21:05:49 +0100

    12

--- api/genre.js
+++ api/genre.js
@@ -9,7 +9,8 @@ module.exports.findAll = (req, res, next) => {
 }
 
 module.exports.findById = ({ params }, res, next) => {
-	Genre.findById(params.id).then( api.sendResponseAndNext(res, next) );
+	Genre.findById(params.id).then( api.sendResponseAndNext(res, next) )
+	.catch( api.sendResponseAndNext(res, next)([]) );
 }
 
 module.exports.create = ({ params }, res, next) => {
