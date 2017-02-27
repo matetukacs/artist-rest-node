4d6686250347a465fb7dd2aecb48c9ff3c05be5a
Mate Tukacs <matt.tukacs@gmail.com>
2017-02-27 05:57:57 +0100

    12

--- index.js
+++ index.js
@@ -17,19 +17,39 @@ server.listen(process.env.PORT || 5000, () => {
 });
 
 mongoose.Promise = global.Promise;
-// assert.equal(query.exec().constructor, global.Promise);
 
-var ArtistSchema = new Schema({
+const ArtistSchema = new Schema({
   id: mongoose.Schema.ObjectId,
   name: String,
   genres: Array,
   albums: Array
 });
 
+const GenreSchema = new Schema({
+  id: mongoose.Schema.ObjectId,
+  name: String
+});
+
+const AlbumSchema = new Schema({
+  id: mongoose.Schema.ObjectId,
+  name: String,
+  imageUrl, String
+});
+
 const Artist = mongoose.model('Artist', ArtistSchema); 
 
 server.get('/artists', (a, res, next) => {
 	Artist.find().then( data => res.send(data) );
+});
+
+server.post('/artist', ({ params }, res, next) => {
+
+	const newArtist = new Artist();
+	newArtist.name = body.name;
+	newArtist.genres = body.genres;
+	newArtist.albums = body.albums;
+
+	newArtist.save().then( data => res.send(data) );
 });
 
 server.get('/artist/:id', ({ params }, res, next) => {
