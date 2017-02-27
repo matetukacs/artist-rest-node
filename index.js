d3371ac528aff15bc8ee4b395a886c099310c288
Mate Tukacs <matt.tukacs@gmail.com>
2017-02-27 17:53:00 +0100

    12

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ server.post('/album', ({ params, files }, res, next) => {
 	
 	const fileStoreUrl = 'https://www.filestackapi.com/api/store/S3?key='+config.creds.filestack_api_key+'container='+config.filestack.albums_container;
 
-	r.postRequest(fileStoreUrl, {fileUpload: files.image, container: })
+	r.postRequest(fileStoreUrl, {fileUpload: files.image})
 	.then(console.log);
 
 	//  ( { url } ) => saveAlbum(params.name)(url) )
