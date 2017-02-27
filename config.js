e371cd18abae350ca3a5514c115924feb43a2a48
Mate Tukacs <matt.tukacs@gmail.com>
2017-02-27 18:13:11 +0100

    12

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
 exports.mongoose = {
-  auth: 'mongodb://heroku_xjcf432g:5jrl1mbdgpjbsb2cjqnolpn6tf@ds157799.mlab.com:57799/heroku_xjcf432g',
+  auth: 'mongodb://heroku_xjcf432g:5jrl1mbdgpjbsb2cjqnolpn6tf@ds157799.mlab.com:57799/heroku_xjcf432g'
 }
 
 exports.filestack = {
-	const api_key=  'A09oJINhT6GQP9jv7ppXAz';
-	const store_url = 'https://www.filestackapi.com/api/store/S3';
-	const album_container = 'album-covers';
-	const access = 'public';
-
+	api_key : 'A09oJINhT6GQP9jv7ppXAz',
+	store_url : 'https://www.filestackapi.com/api/store/S3',
+	album_container : 'album-covers';
+	access : 'public',
 	file_store_url : `${store_url}?key=${api_key}&container=${album_container}&access=${access}`
 }
