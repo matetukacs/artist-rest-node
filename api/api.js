e81bccbf5fef345735334d54da04ab69e2d39a26
Mate Tukacs <matt.tukacs@gmail.com>
2017-02-27 19:52:43 +0100

    12

--- api/api.js
+++ api/api.js
@@ -1,13 +1,10 @@
 
 module.exports.common = {
-	sendResponseAndNext : (res, next) => _.flow(sendResponse(res), next);
-
+	sendResponseAndNext : (res, next) => _.flow(sendResponse(res), next),
 	sendResponse : (res) => {
 		return (data = "") => res.send(data);
-	}
-
-	sendEmptyResponseAndNext : (res, next) => _.flow(sendEmptyResponse(res), next);
-
+	},
+	sendEmptyResponseAndNext : (res, next) => _.flow(sendEmptyResponse(res), next),
 	sendEmptyResponse : (res) => {
 		return () => res.send();
 	}	
