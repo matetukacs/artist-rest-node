e6ff951b4d7df8c9dbc14ab76fab23759339d731
Mate Tukacs <matt.tukacs@gmail.com>
2017-02-27 21:37:38 +0100

    test project done

---
+++ config.js
@@ -0,0 +1,9 @@
+exports.mongoose = {
+  auth: 'mongodb://heroku_xjcf432g:5jrl1mbdgpjbsb2cjqnolpn6tf@ds157799.mlab.com:57799/heroku_xjcf432g'
+}
+
+const fs_api_key=  'A09oJINhT6GQP9jv7ppXAz';
+const fs_store_url = 'https://www.filestackapi.com/api/store/S3';
+const fs_album_container = 'album-covers';
+const fs_access = 'public';
+
