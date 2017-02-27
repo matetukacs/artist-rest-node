15cff0030e6f59290192d51ce040c3e96e09e4e6
Mate Tukacs <matt.tukacs@gmail.com>
2017-02-27 20:01:19 +0100

    12

--- api/api.js
+++ api/api.js
@@ -1,11 +1,15 @@
+const _ = require('lodash');
 
 module.exports.common = {
-	sendResponseAndNext : (res, next) => _.flow(sendResponse(res), next),
-	sendResponse : (res) => {
+	sendResponseAndNext = (res, next) => _.flow(sendResponse(res), next),
+
+	sendResponse = (res) => {
 		return (data = "") => res.send(data);
 	},
-	sendEmptyResponseAndNext : (res, next) => _.flow(sendEmptyResponse(res), next),
-	sendEmptyResponse : (res) => {
+
+ 	sendEmptyResponseAndNext = (res, next) => _.flow(sendEmptyResponse(res), next),
+
+	sendEmptyResponse = (res) => {
 		return () => res.send();
 	}	
 }
