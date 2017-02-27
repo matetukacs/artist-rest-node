822d5c56c1809790a9e22ba6bf860e9b88dfd9aa
Mate Tukacs <matt.tukacs@gmail.com>
2017-02-27 20:20:14 +0100

    12

--- api/genre.js
+++ api/genre.js
@@ -1,31 +1,32 @@
 const { model } = require('mongoose');
 const schema = require('./../schema');
+const api = require('./api');
 
 const Genre = model('Genre', schema.Genre); 
 
 module.exports.findAll = (req, res, next) => {
-	Genre.find().then( sendResponseAndNext(res, next) );
+	Genre.find().then( api.sendResponseAndNext(res, next) );
 }
 
 module.exports.findById = ({ params }, res, next) => {
-	Genre.findById(params.id).then( sendResponseAndNext(res, next) );
+	Genre.findById(params.id).then( api.sendResponseAndNext(res, next) );
 }
 
 module.exports.create = ({ params }, res, next) => {
 	const newGenre = new Genre();
 	newGenre.name = params.name;
 
-	newGenre.save().then( sendEmptyResponseAndNext(res, next) );
+	newGenre.save().then( api.sendEmptyResponseAndNext(res, next) );
 }
 
 module.exports.update = ({ params, body }, res, next) => {
 	Genre.findById(params.id).then( genre => {
 		genre.name = body.name;
 
-		genre.save().then( sendEmptyResponseAndNext(res, next) );
+		genre.save().then( api.sendEmptyResponseAndNext(res, next) );
 	});
 }
 
 module.exports.delete = ({ params }, res, next) => {
-	Genre.findByIdAndRemove(params.id).then( sendEmptyResponseAndNext(res, next) );
+	Genre.findByIdAndRemove(params.id).then( api.sendEmptyResponseAndNext(res, next) );
 }
